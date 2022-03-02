eb0e9de656cc9fdcb89ee3e648ed3dd6af9831e2
iwate <github@iwate.me>
2022-03-02 10:02:38 +0900

    add shared lib to build wasm workflow

--- lib/build-lib.sh
+++ lib/build-lib.sh
@@ -2,7 +2,7 @@
 
 apt-get update -y
 apt-get install -y tzdata 
-apt-get install -y build-essential cmake curl python3 python3-pip
+apt-get install -y build-essential cmake curl python3 python3-pip libxkbcommon-x11-0
 
 cd /src/wasi-lab/qjs-wasi/
 ./build.sh
