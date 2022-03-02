f9b475375a268ff049b968c0a2ff01e77fe64c94
iwate <github@iwate.me>
2022-03-02 09:57:08 +0900

    add python to build wasm workflow

--- lib/build-lib.sh
+++ lib/build-lib.sh
@@ -2,7 +2,7 @@
 
 apt-get update -y
 apt-get install -y tzdata 
-apt-get install -y build-essential cmake curl
+apt-get install -y build-essential cmake curl python3 python3-pip
 
 cd /src/wasi-lab/qjs-wasi/
 ./build.sh
